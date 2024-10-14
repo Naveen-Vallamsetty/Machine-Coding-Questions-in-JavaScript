df58295b455561e3e17c926f8ba0929c2f42f421
Naveen Vallamsetty <151658754+Naveen-Vallamsetty@users.noreply.github.com>
2024-10-14 17:17:40 +0530

    reverse string

---
+++ Basic-Concepts/01_reverse_a_string.js
@@ -0,0 +1,32 @@
+/*
+    Implement a function to reverse a string
+    Srring: "Javascript"
+    Reverse string: "Tpircsavaj"
+
+    Make first letter capital in reverse string 
+*/
+
+let reverseString = (str) => {
+  let reverseString = str.split("").reverse().join("");
+  reverseString =
+    reverseString.charAt(0).toUpperCase() +
+    reverseString.slice(1).toLowerCase();
+
+  return reverseString;
+};
+
+console.log(reverseString("JavaScript"));
+
+// let reverseString = (str) => {
+//   let reversedString = "";
+//   //   str = str.slice(-1).toUpperCase();
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversedString += str[i];
+//     reversedString =
+//       reversedString.charAt(0).toUpperCase() +
+//       reversedString.slice(1).toLowerCase();
+//   }
+//   return reversedString;
+// };
+
+// console.log(reverseString("JavaScript"));
