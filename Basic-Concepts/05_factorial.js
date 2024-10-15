5dcc5926b7b75a638ca17922da8b4f4690ae19fa
Naveen Vallamsetty <151658754+Naveen-Vallamsetty@users.noreply.github.com>
2024-10-15 16:48:15 +0530

    factorial of a number

--- Basic-Concepts/05_factorial.js
+++ Basic-Concepts/05_factorial.js
@@ -18,3 +18,20 @@ let factorial = (n) => {
 };
 
 console.log(factorial(5));
+
+/*
+Iterative Approach
+
+let factorialIterative = (num) => {
+  if (num === 0) return 1;
+  else if (num < 0) return "not a valid number";
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+};
+
+console.log(factorialIterative(4));
+
+*/
